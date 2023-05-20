8c5affea6d6ad818cae88e5d806b77eea7279a60
y0taa <volodymyr.nadolin@gmail.com>
2023-05-20 23:13:07 +0300

    added firebase

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.btnRegister.setOnClickListener {
             Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
             val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
